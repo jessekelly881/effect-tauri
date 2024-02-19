302ddfb0a070c819940ab4cd4a3bec3feda379d2
Jesse Kelly <kellyjesse881@gmail.com>
2024-02-19 15:47:12 -0600

    feat: added basic fns

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# ts-lib
-Barebones typescript library boilerplate.
+# effect-tauri
+
+A simple of wrappers around the core tauri api. Currently provides wrappers for:
+
+- listen :: Stream
+- invoke :: Effect
+- emit   :: Effect
+
+More to come.
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,47 @@
 /**
  * @since 1.0.0
  */
+import * as Api from "@tauri-apps/api";
+import * as Event from "@tauri-apps/api/event";
 
-import { a } from "@/test";
+import { Chunk, Data, Effect, Scope, Stream, StreamEmit } from "effect";
 
-/**
- * @since 1.0.0
- */
-export const index = a;
+export class TauriError extends Data.TaggedError("TauriError")<{
+	message: string;
+}> {}
+
+export const listen = (name: string) =>
+	Stream.asyncScoped(
+		(
+			emit: StreamEmit.Emit<never, TauriError, Event.Event<unknown>, void>
+		) =>
+			Effect.gen(function* (_) {
+				const scope = yield* _(Scope.Scope);
+				const res = yield* _(
+					Effect.tryPromise(() =>
+						Event.listen(name, (event) =>
+							emit(
+								Effect.succeed(
+									Chunk.of(event as Event.Event<unknown>)
+								)
+							)
+						)
+					),
+					() => new TauriError({ message: "" })
+				);
+
+				yield* _(Scope.addFinalizer(scope, Effect.succeed(res)));
+			})
+	);
+
+export const invoke = (cmd: string) =>
+	Effect.tryPromise({
+		try: () => Api.invoke(cmd),
+		catch: () => new TauriError({ message: "" })
+	});
+
+export const emit = <T>(name: string, payload: T) =>
+	Effect.tryPromise({
+		try: () => Event.emit(name, payload),
+		catch: () => new TauriError({ message: "" })
+	});

7eb7313eec916ffa9efae51e1979c1776da88740
Jesse Kelly <kellyjesse881@gmail.com>
2024-02-19 15:51:28 -0600

    chore: added jsdoc comments

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,16 @@ import * as Event from "@tauri-apps/api/event";
 
 import { Chunk, Data, Effect, Scope, Stream, StreamEmit } from "effect";
 
+/**
+ * @since 1.0.0
+ */
 export class TauriError extends Data.TaggedError("TauriError")<{
 	message: string;
 }> {}
 
+/**
+ * @since 1.0.0
+ */
 export const listen = (name: string) =>
 	Stream.asyncScoped(
 		(
@@ -34,12 +40,18 @@ export const listen = (name: string) =>
 			})
 	);
 
+/**
+ * @since 1.0.0
+ */
 export const invoke = (cmd: string) =>
 	Effect.tryPromise({
 		try: () => Api.invoke(cmd),
 		catch: () => new TauriError({ message: "" })
 	});
 
+/**
+ * @since 1.0.0
+ */
 export const emit = <T>(name: string, payload: T) =>
 	Effect.tryPromise({
 		try: () => Event.emit(name, payload),
